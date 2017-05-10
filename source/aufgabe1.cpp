bd02438d406c4bc96fd594b19f1d5b7c32d3a879
toratios <max.pagel@uni-weimar.de>
2017-05-10 16:13:16 +0200

    fixed spacing, added aufgabe2bis4

--- source/aufgabe1.cpp
+++ source/aufgabe1.cpp
@@ -1,9 +1,9 @@
-#include <cstdlib>      // std :: rand ()
-#include <vector>       // std :: vector < >
-#include <list>         // std :: list < >
-#include <iostream>     // std :: cout
-#include <iterator>     // std :: ostream_iterator < >
-#include <algorithm>    // std :: reverse , std :: generate
+#include <cstdlib>      // std::rand()
+#include <vector>       // std::vector<>
+#include <list>         // std::list<>
+#include <iostream>     // std::cout
+#include <iterator>     // std::ostream_iterator<>
+#include <algorithm>    // std::reverse , std::generate
 
 
 int main ()
@@ -13,24 +13,20 @@ int main ()
     for (auto& v : v0) {
         v = std::rand();
     }
-    std::copy ( std :: begin ( v0 ) , std :: end ( v0 ) ,
-    std::ostream_iterator <int>( std :: cout , "\n" ));
+    std::copy(std::begin(v0), std::end(v0), std::ostream_iterator <int>(std::cout, "\n"));
 
-    std::list <int> l0 ( v0 . size ());
-    std::copy ( std :: begin ( v0 ) , std :: end ( v0 ) , std :: begin ( l0 ));
+    std::list<int>l0(v0.size());
+    std::copy(std::begin(v0), std::end(v0), std::begin(l0));
 
-    std::list <int> l1 ( std :: begin ( l0 ) , std :: end ( l0 ));
-    std::reverse ( std :: begin ( l1 ) , std :: end ( l1 ));
-    std::copy ( std :: begin ( l1 ) , std :: end ( l1 ) ,
-    std::ostream_iterator <int>( std :: cout , "\n" ));
+    std::list<int>l1(std::begin(l0), std::end(l0));
+    std::reverse(std::begin(l1), std::end(l1));
+    std::copy(std::begin(l1), std::end(l1), std::ostream_iterator<int>(std::cout, "\n"));
 
     l1.sort();
-    std::copy ( l1 . begin () , l1 . end () ,
-    std::ostream_iterator <int>( std :: cout , "\n" ));
+    std::copy(l1.begin(), l1.end(), std::ostream_iterator<int>(std::cout, "\n"));
 
-    std::generate ( std :: begin ( v0 ) , std :: end ( v0 ) , std :: rand );
-    std::copy ( v0 . rbegin () , v0 . rend () ,
-    std::ostream_iterator <int>( std :: cout , "\n" ));
+    std::generate(std::begin(v0), std::end(v0), std::rand);
+    std::copy(v0.rbegin(), v0.rend(), std::ostream_iterator<int>(std::cout, "\n"));
 
     return 0;
 }
