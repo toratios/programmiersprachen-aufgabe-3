627f3a8ccc4db5b83403d2678deabf7eb95e14b6
toratios <max.pagel@uni-weimar.de>
2017-05-15 16:49:10 +0200

    3.5 working

---
+++ source/aufgabe5.cpp
@@ -0,0 +1,41 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+#include <cmath>
+#include <algorithm>
+
+bool is_multiple_of_3(unsigned int x){
+  if(x % 3 == 0){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+bool is_not_multiple_of_3(unsigned int x){
+  if(x % 3 == 0){
+    return false;
+  }
+  else{
+    return true;
+  }
+}
+
+TEST_CASE("filter alle vielfache von drei", "[erase]")
+{
+  std::vector<unsigned int> v(100);
+  for(auto& vit : v){
+    vit = std::rand() %101;
+  }
+  v.erase(std::remove_if(v.begin(), v.end(), is_not_multiple_of_3), v.end());
+
+  std::copy(std::begin(v), std::end(v), 
+    std::ostream_iterator <unsigned int>(std::cout, "\n")); 
+
+REQUIRE(std::all_of(v.begin(), v.end(), is_multiple_of_3));
+}
+
+int main(int argc, char * argv[])
+{
+    return Catch::Session().run(argc, argv);
+}
