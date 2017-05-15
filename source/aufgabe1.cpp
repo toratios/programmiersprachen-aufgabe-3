5c1fbbef96b838396a4e96ae94722721579a718b
toratios <max.pagel@uni-weimar.de>
2017-05-15 16:00:49 +0200

    3.3 working

--- source/aufgabe1.cpp
+++ source/aufgabe1.cpp
@@ -8,25 +8,29 @@
 
 int main ()
 {
-    std::vector <int> v0 (10);
+std::vector <int> v0 (10);
 
-    for (auto& v : v0) {
-        v = std::rand();
-    }
-    std::copy(std::begin(v0), std::end(v0), std::ostream_iterator <int>(std::cout, "\n"));
+  for (auto& v : v0) {
+    v = std::rand();
+  }
+  std::copy(std::begin(v0), std::end(v0), 
+    std::ostream_iterator <int>(std::cout, "\n"));
 
-    std::list<int>l0(v0.size());
-    std::copy(std::begin(v0), std::end(v0), std::begin(l0));
+  std::list<int>l0(v0.size());
+  std::copy(std::begin(v0), std::end(v0), std::begin(l0));
 
-    std::list<int>l1(std::begin(l0), std::end(l0));
-    std::reverse(std::begin(l1), std::end(l1));
-    std::copy(std::begin(l1), std::end(l1), std::ostream_iterator<int>(std::cout, "\n"));
+  std::list<int>l1(std::begin(l0), std::end(l0));
+  std::reverse(std::begin(l1), std::end(l1));
+  std::copy(std::begin(l1), std::end(l1), 
+    std::ostream_iterator<int>(std::cout, "\n"));
 
-    l1.sort();
-    std::copy(l1.begin(), l1.end(), std::ostream_iterator<int>(std::cout, "\n"));
+  l1.sort();
+  std::copy(l1.begin(), l1.end(), 
+    std::ostream_iterator<int>(std::cout, "\n"));
 
-    std::generate(std::begin(v0), std::end(v0), std::rand);
-    std::copy(v0.rbegin(), v0.rend(), std::ostream_iterator<int>(std::cout, "\n"));
+  std::generate(std::begin(v0), std::end(v0), std::rand);
+  std::copy(v0.rbegin(), v0.rend(), 
+    std::ostream_iterator<int>(std::cout, "\n"));
 
-    return 0;
+  return 0;
 }
--- source/aufgabe2bis4.cpp
+++ source/aufgabe2bis4.cpp
@@ -1,17 +1,42 @@
-#include <vector>       // std::vector<>
-#include <list>         // std::list<>
-#include <cstdlib>      // std::rand()
-#include <iostream>     // std::cout
-#include <iterator>     // std::ostream_iterator<>
+#include <vector>   // std::vector<>
+#include <list>     // std::list<>
+#include <cstdlib>  // std::rand()
+#include <iostream> // std::cout
+#include <iterator> // std::ostream_iterator<>
+#include <set>      // std::set<>
 
 int main ()
 {
-    std::list<unsigned int> l0(100);
-    for(auto& l : l0){
-        l = std::rand() %101;
-    }
-    std::vector<unsigned int> v0(l0.size());
-    std::copy(std::begin(l0), std::end(l0), std::begin(v0));
+  std::list<unsigned int> l0(100);
+  for(auto& l : l0){
+    l = std::rand() %101;
+  }
+  
+  std::vector<unsigned int> v0(l0.size());
+  std::copy(std::begin(l0), std::end(l0), std::begin(v0));
 
-    std::copy(std::begin(l0), std::end(l0), std::ostream_iterator <unsigned int>(std::cout, "\n"));   
+  //std::copy(std::begin(l0), std::end(l0), 
+  //  std::ostream_iterator <unsigned int>(std::cout, "\n")); 
+
+  std::set<unsigned int> s0;
+  for(auto& l : l0){
+    s0.insert(l);
+  }
+
+  std::set<unsigned int> s1;
+  for(int i = 0; i <= 100; ++i){
+    s1.insert(i);
+  }
+
+  for(auto& s : s0){
+    s1.erase(s1.find(s));
+  }
+
+  std::copy(std::begin(s0), std::end(s0), 
+    std::ostream_iterator <unsigned int>(std::cout, "\n")); 
+  
+  std::cout << "Number of elements in s0: " << s0.size() << "\n";
+
+    std::copy(std::begin(s1), std::end(s1), 
+    std::ostream_iterator <unsigned int>(std::cout, "\n")); 
 }
