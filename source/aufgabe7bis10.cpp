b336e949328eb901444d1659b919d58c281c39d9
toratios <max.pagel@uni-weimar.de>
2017-05-18 14:39:15 +0200

    3.10 working

--- source/aufgabe7bis10.cpp
+++ source/aufgabe7bis10.cpp
@@ -30,7 +30,6 @@ TEST_CASE("Lamdbda sort","[sort]")
   REQUIRE(std::is_sorted(vec_c.begin(), vec_c.end()));
 }
 
-
 TEST_CASE("Lambda Addition","[add]"){
 
   std::vector<int> v1{1,2,3,4,5,6,7,8,9};
@@ -40,6 +39,31 @@ TEST_CASE("Lambda Addition","[add]"){
   std::transform(v1.begin(), v1.end(), v2.begin(), v3.begin(),[](int a,int b){return a + b;});
 
   REQUIRE(std::all_of(v3.begin(), v3.end(), [](int a){return a == 10;}));
+}
+
+bool is_even(int n){
+  return n % 2 == 0;
+  }
+
+
+template <typename Container, typename Praedikat>
+Container filter(Container const& cont, Praedikat const& pr){
+	Container res_cont;
+	for (auto& c : cont) {
+		if(pr(c)){
+			res_cont.push_back(c);
+		}
+	}
+	return res_cont;
+}
+
+
+TEST_CASE("filter alle ungeraden", "[filter]")
+{
+  std::vector<int> v{1,2,3,4,5,6};
+  std::vector<int> alleven = filter(v,is_even); 
+
+  REQUIRE(std::all_of(alleven.begin(), alleven.end(), is_even));
 }
 
 
