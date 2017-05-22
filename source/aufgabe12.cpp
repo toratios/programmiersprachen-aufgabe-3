f2ae051037e7ce4cc6246357cd68d36cc4f8233b
toratios <max.pagel@uni-weimar.de>
2017-05-22 15:25:02 +0200

    aufgabe12 working

--- source/aufgabe12.cpp
+++ source/aufgabe12.cpp
@@ -6,16 +6,13 @@
 #include "vec2.hpp"
 
 
-
-
-
 TEST_CASE("copy_Circle", "[copy]")
 {
 std::vector<Circle> circles{{5.0f},{3.0f},{8.0f},{1.0f},{5.0f}};
-std::vector<Circle> circles_copy(circles.size());
+std::vector<Circle> circles_filter(circles.size());
 
-auto it = std::copy_if(circles.begin(), circles.end(), circles_copy.begin(), [](Circle a){return a > 4.0f;});
-circles_copy.resize(std::distance(circles_copy.begin(),it));
+auto it = std::copy_if(circles.begin(), circles.end(), circles_filter.begin(), [](Circle a){return a > 4.0f;});
+circles_filter.resize(std::distance(circles_copy.begin(),it));
 REQUIRE(std::all_of(circles_copy.begin(), circles_copy.end(), [](Circle a){return a > 3.0f;}));
 
 }
