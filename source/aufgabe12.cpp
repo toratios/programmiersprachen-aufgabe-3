9c55a94a1bcaa52e7be3160789dc3738cbd1d738
toratios <max.pagel@uni-weimar.de>
2017-05-22 15:40:04 +0200

    cleanup

--- source/aufgabe12.cpp
+++ source/aufgabe12.cpp
@@ -8,13 +8,12 @@
 
 TEST_CASE("copy_Circle", "[copy]")
 {
-std::vector<Circle> circles{{5.0f},{3.0f},{8.0f},{1.0f},{5.0f}};
-std::vector<Circle> circles_filter(circles.size());
+  std::vector<Circle> circles{{5.0f},{3.0f},{8.0f},{1.0f},{5.0f}};
+  std::vector<Circle> circles_filter(circles.size());
 
-auto it = std::copy_if(circles.begin(), circles.end(), circles_filter.begin(), [](Circle a){return a > 4.0f;});
-circles_filter.resize(std::distance(circles_copy.begin(),it));
-REQUIRE(std::all_of(circles_copy.begin(), circles_copy.end(), [](Circle a){return a > 3.0f;}));
-
+  auto it = std::copy_if(circles.begin(), circles.end(), circles_filter.begin(), [](Circle a){return a > 4.0f;});
+  circles_filter.resize(std::distance(circles_filter.begin(),it));
+  REQUIRE(std::all_of(circles_filter.begin(), circles_filter.end(), [](Circle a){return a > 3.0f;}));
 }
 
 int main(int argc, char* argv [])
